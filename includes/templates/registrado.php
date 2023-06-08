fbbde59f0139393f2d813eca28f5aede645cff1a
Jorge Rodriguez <52306657+JorgeNoh@users.noreply.github.com>
2023-06-08 10:58:28 -0600

    Add files via upload

---
+++ includes/templates/registrado.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <title>El Blog</title>
+</head>
+<body>
+		<nav class="navegacion">
+				<ul>
+				<li><a href="index">Inicio</a></li>
+				<li><a href="">Sobre <i class="fas fa-chevron-down"></i></a>
+					<ul>
+						<li><a href="nosotros">Nosotros </a></li>
+						<li><a href="nosotros #vision">Nuestra Vision</a></li>    
+					</ul>     
+				</li>
+				<li> <a href="contacto">Contacto</a></li>
+				<li> <a href="#"><?= $user['nombre'] ?> <i class="fas fa-chevron-down"></i></a>
+				    <ul>
+						<li><a href="#"><?= $user['email'] ?></a></li>
+					</ul>  
+				</li>
+				<li> <a href="logout">Cerrar Sesión</a></li>
+			</ul>
+			</nav>
+			<br> <br> <br> <br>
