5740602225af622e622f3b7d2535895efec172dc
Jorge Rodriguez <52306657+JorgeNoh@users.noreply.github.com>
2023-06-08 10:56:52 -0600

    Create footer.php

---
+++ includes/templates/footer.php
@@ -0,0 +1,12 @@
+<aside>
+			<h2 class="tit">Síguenos en nuestras redes sociales</h2>
+			<ul>
+				<li><i class="fab fa-facebook-square" id="fb"></i><a href="#">Facebook</a></li>
+				<li><i class="fab fa-twitter" id="twitter"></i><a href="#">Twitter</a></li>
+				<li><i class="fab fa-youtube" id="yt"></i><a href="#">Youtube</a></li>
+			</ul>
+		</aside>
+	</div>
+
+</body>
+</html>
